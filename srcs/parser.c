da6abfcdb3c9c928ed99ce998a6c714358a6db92
Alexandre PION <apion@student.42.fr>
2019-04-24 18:48:17 +0200

    Handle contextualized error in parser, add lines to list

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 15:57:48 by apion             #+#    #+#             */
-/*   Updated: 2019/04/24 16:19:03 by apion            ###   ########.fr       */
+/*   Updated: 2019/04/24 18:20:25 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,16 @@ static int	get_number_ants(t_env *env)
 	char	*line;
 	int		len;
 	int		eol_had_newline;
+	int		status;
 
 	len = get_next_line(STDIN_FILEN0, &line, &eol_had_newline);
 	if (len == GNL_ERROR)
 		return (ERR_READ);
-	t_env->ants = 0;
-	return (0);
+	status = atoi_pos(line, &(env->nb_ants));
+	if (status == SUCCESS)
+		if (list_line_add_first(&env->map, line))
+			return (errno);
+	return (status);
 }
 
 
@@ -34,11 +38,17 @@ int		parser(t_env *env)
 	char	*line;
 	int		len;
 	int		eol_had_newline;
+	int		status;
 
-	if (get_number_ants(env) < 0)
-		return (ERR_ANTS);
+	if ((status = get_number_ants(env)) != SUCCESS)
+		return (status);
 	while ((len = get_next_line(STDIN_FILEN0, &line, &eol_had_newline)) >= 0)
 	{
+		list_add_first(&env->map, line);
 	}
-	return (0);
+	if (!eol_had_newline)
+		return (ERR_EOF_NO_NEWLINE)
+	if (len == GNL_ERROR)
+		return (errno);
+	return (SUCCESS);
 }
