62d447fd039cb21467a39a9c2b1c29d5752cd23d
Alexandre PION <apion@student.42.fr>
2019-04-24 18:19:35 +0200

    Add room structure

--- incs/room.h
+++ incs/room.h
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env.h                                              :+:      :+:    :+:   */
+/*   room.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/24 16:03:28 by apion             #+#    #+#             */
-/*   Updated: 2019/04/24 17:43:17 by apion            ###   ########.fr       */
+/*   Created: 2019/04/24 17:39:52 by apion             #+#    #+#             */
+/*   Updated: 2019/04/24 18:07:59 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ENV_H
-# define ENV_H
+#ifndef ROOM_H
+# define ROOM_H
 
-# include "list_line.h"
-# include "room.h"
-
-typedef struct s_env	t_env;
-struct	s_env
+typedef struct s_room	t_room;
+struct	s_room
 {
-	int			nb_ants;
-	t_list_line	*map;
-	t_room		*start;
-	t_room		*end;
-};
+	char	*name;
+	int		x;
+	int		y;
+	int		is_empty;
+}
+
+#endif
