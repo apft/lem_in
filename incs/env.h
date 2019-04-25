275ffd9e3a0c370e8f80abfe9d0c1b451cb85d34
Jalil KETTANI <jkettani@e2r2p3.42.fr>
2019-04-25 18:24:57 +0200

    Continue room parsing

--- incs/env.h
+++ incs/env.h
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 16:03:28 by apion             #+#    #+#             */
-/*   Updated: 2019/04/25 15:01:27 by apion            ###   ########.fr       */
+/*   Updated: 2019/04/25 18:14:08 by jkettani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ struct	s_env
 	int			nb_ants;
 	t_list_line	*lines;
 	t_list		*map;
+	t_room		**rooms_array;
 	int			nb_room;
 	t_room		*start;
 	t_room		*end;
--- srcs/parser_checker.c
+++ srcs/parser_checker.c
@@ -6,7 +6,7 @@
 /*   By: jkettani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 11:24:44 by jkettani          #+#    #+#             */
-/*   Updated: 2019/04/25 15:28:56 by apion            ###   ########.fr       */
+/*   Updated: 2019/04/25 18:24:21 by jkettani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,25 @@
 #include "error.h"
 #include "parser.h"
 
+static int	get_room_name(t_list *node)
+{
+	return (((t_room *)node->content)->name);
+}
+
+static int	lst_to_array(t_env *env)
+{
+	return (SUCCESS);
+}
+
 static int	handle_tube(char *line, t_env *env, unsigned int *cmd_flag)
 {
 	if (*cmd_flag & BLK_ROOM)
 	{
 		*cmd_flag ^= BLK_ROOM | BLK_TUBE;
-		// create sorted room array
+		if (lst_to_array(env) != SUCCESS)
+			return (ERR_LST_TO_ARRAY_FAILED);
+		// not good
+		ft_strarraymergesort(env->rooms_array, env->nb_room);
 	}
 	return (SUCCESS);
 }
