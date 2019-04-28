830b3c8734ec9953790bd0b0fe8a931661e7b213
Alexandre PION <apion@student.42.fr>
2019-04-28 14:20:29 +0200

    Populate adjacency matrix when parsing a tube

--- srcs/parser_tube.c
+++ srcs/parser_tube.c
@@ -6,7 +6,7 @@
 /*   By: jkettani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 11:24:44 by jkettani          #+#    #+#             */
-/*   Updated: 2019/04/28 13:49:36 by apion            ###   ########.fr       */
+/*   Updated: 2019/04/28 14:19:53 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,8 @@ int			handle_tube(char *line, t_env *env)
 	id_room2 = ft_bsearch_id(line + id_dash + 1, &((t_array_args){env->rooms_array, sizeof(t_room *), env->nb_room}), &cmp_room2);
 	if (id_room1 < 0 || id_room2 < 0)
 		return (ERR_ROOM_DOES_NOT_EXIST);
+	env->matrix[id_room1][id_room2] += 1;
+	env->matrix[id_room2][id_room1] += 1;
 
 	get_room_name(env->map);
 	return (SUCCESS);
