e15db38ad389150d1863c6d63dbfb480759ddfa6
Alexandre PION <apion@student.42.fr>
2019-04-29 18:45:05 +0200

    Display Dijkstra value in output

--- srcs/output/output.c
+++ srcs/output/output.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 18:06:53 by apion             #+#    #+#             */
-/*   Updated: 2019/04/29 12:34:15 by apion            ###   ########.fr       */
+/*   Updated: 2019/04/29 18:28:15 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@ void		print_output(t_env *env)
 	print_lines(env->lines);
 	ft_putchar('\n');
 	print_matrix(env);
+	ft_printf("dijkstra: %d\n", env->end->dst_min_to_start);
 }
