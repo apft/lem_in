43df78d9688da0e0032b87f47e0f0899c490fc69
Alexandre PION <apion@student.42.fr>
2019-04-30 17:26:17 +0200

    Store only one tube between 2 rooms

--- srcs/parser_tube.c
+++ srcs/parser_tube.c
@@ -6,7 +6,7 @@
 /*   By: jkettani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 11:24:44 by jkettani          #+#    #+#             */
-/*   Updated: 2019/04/29 16:39:04 by apion            ###   ########.fr       */
+/*   Updated: 2019/04/30 17:25:30 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int			handle_tube(char *line, t_env *env)
 	id_room2 = get_room_id_by_name(line + id_dash + 1, env, ROOM_NAME_ONLY);
 	if (id_room1 < 0 || id_room2 < 0)
 		return (ERR_ROOM_DOES_NOT_EXIST);
-	env->matrix[id_room1][id_room2] += 1;
-	env->matrix[id_room2][id_room1] += 1;
+	env->matrix[id_room1][id_room2] = 1;
+	env->matrix[id_room2][id_room1] = 1;
 	return (SUCCESS);
 }
