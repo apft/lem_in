226c28df151df1ea2728f961bbf9bfa73cd46044
Alexandre PION <apion@student.42.fr>
2019-05-20 15:55:24 +0200

    Display augmenting paths

--- srcs/output/output.c
+++ srcs/output/output.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 18:06:53 by apion             #+#    #+#             */
-/*   Updated: 2019/05/01 13:30:55 by apion            ###   ########.fr       */
+/*   Updated: 2019/05/20 15:49:49 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,35 @@
 #include "ft_printf.h"
 #include "output.h"
 
+static void	print_path(t_env *env, t_room *start)
+{
+	ft_printf("%s-", env->start->name);
+	while (start != env->end)
+	{
+		ft_printf("%s-", start->name);
+		start = start->next;
+	}
+	ft_printf("%s\n", env->end->name);
+}
+
+static void	print_paths(t_env *env)
+{
+	int		i;
+
+	i = 0;
+	while (i < env->nb_room)
+	{
+		if (env->matrix[env->start->id][i] && (env->rooms_array[i]->flag & FL_CLOSE_PATH))
+			print_path(env, env->rooms_array[i]);
+		++i;
+	}
+}
+
 void		print_output(t_env *env)
 {
 	print_lines(env->lines);
 	ft_putchar('\n');
 	print_matrix(env);
 	ft_printf("length of shortest path start->end: %d\n", env->end->dst_min_to_start);
+	print_paths(env);
 }
