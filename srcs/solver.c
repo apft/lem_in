370f668fc58c88ae6a115e1d8959d7b0582dbd63
Alexandre PION <apion@student.42.fr>
2019-05-24 08:36:33 +0200

    Refactor solver
    Initialize BFS in another function
    Use a foreach function to apply on each room linked to a reference room.

--- srcs/solver.c
+++ srcs/solver.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 13:43:55 by apion             #+#    #+#             */
-/*   Updated: 2019/05/23 22:03:49 by apion            ###   ########.fr       */
+/*   Updated: 2019/05/24 08:35:01 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,33 @@
 static int	is_closed_path(t_room *room)
 {
 	return (room->flag & FL_CLOSE_PATH);
+}
+
+static int	has_oriented_tube_between_rooms(int id_room_a, int id_room_b, t_env *env)
+{
+	return (env->matrix[id_room_a][id_room_b]);
+}
+
+static int	is_room_already_visited(t_room *room)
+{
+	return (room->visited);
+}
+
+static void	apply_foreach_room_linked_to_ref(t_room *ref, t_env *env, void *data, void (*fct)())
+{
+	t_room	*neighbour;
+	int		i;
+
+	i = 0;
+	while (i < env->nb_room)
+	{
+		if (has_oriented_tube_between_rooms(ref->id, i, env))
+		{
+			neighbour = env->rooms_array[i];
+			fct(ref, env->rooms_array[i], env, data);
+		}
+		++i;
+	}
 }
 
 static void	clear_queue(t_queue *queue)
@@ -68,21 +95,44 @@ static int	save_path(t_env *env)
 	return (SUCCESS);
 }
 
-static int	has_oriented_tube_between_rooms(int id_room_a, int id_room_b, t_env *env)
+static void	search_for_valid_neighbour(t_room *current, t_room *neighbour, t_env *env, t_queue *queue)
 {
-	return (env->matrix[id_room_a][id_room_b]);
-}
-
-static int	is_room_already_visited(t_room *room)
-{
-	return (room->visited);
+	if (is_room_already_visited(neighbour) && neighbour->next != current)
+		return ;
+	if (current == env->start && is_closed_path(neighbour))
+		return ;
+	if (is_closed_path(current))
+	{
+		if (neighbour == current->next)
+			return ;
+		if (!is_closed_path(current->from))
+		{
+			if (neighbour->next != current)
+				return ;
+		}
+		else
+		{
+			if (current->next != current->from)
+				return ;
+		}
+	}
+	else
+	{
+		if (is_closed_path(neighbour) && neighbour->from == env->start)
+			return ;
+	}
+	ft_printf("%s(%s) ", neighbour->name, neighbour->from ? neighbour->from->name : ".");
+	if (neighbour != env->end)
+		neighbour->visited = 1;
+	neighbour->from = current;
+	if (!is_closed_path(neighbour))
+		neighbour->dst = neighbour->from->dst + 1;
+	enqueue(queue, (void *)neighbour);
 }
 
 static void	bfs_max_flow(t_env *env, t_queue *queue)
 {
 	t_room	*current;
-	t_room	*neighbour;
-	int		i;
 
 	while (queue->head)
 	{
@@ -90,74 +140,15 @@ static void	bfs_max_flow(t_env *env, t_queue *queue)
 		if (current->dst >= env->end->dst)
 			continue ;
 		ft_printf("%s(%s)->{", current->name, current->from ? current->from->name : ".");
-		i = 0;
-		while (i < env->nb_room)
-		{
-			if (has_oriented_tube_between_rooms(current->id, i, env))
-			{
-				neighbour = env->rooms_array[i];
-				if (is_room_already_visited(neighbour) && neighbour->next != current)
-				{
-					++i;
-					continue ;
-				}
-				if (current == env->start && is_closed_path(neighbour))
-				{
-					++i;
-					continue ;
-				}
-				if (is_closed_path(current))
-				{
-					if (neighbour == current->next)
-					{
-						++i;
-						continue ;
-					}
-					if (!is_closed_path(current->from))
-					{
-						if (neighbour->next != current)
-						{
-							++i;
-							continue ;
-						}
-					}
-					else
-					{
-						if (current->next != current->from)
-						{
-							++i;
-							continue ;
-						}
-					}
-				}
-				else
-				{
-					if (is_closed_path(neighbour) && neighbour->from == env->start)
-					{
-						++i;
-						continue ;
-					}
-				}
-				ft_printf("%s(%s) ", neighbour->name, neighbour->from ? neighbour->from->name : ".");
-
-				if (neighbour != env->end)
-					neighbour->visited = 1;
-				neighbour->from = current;
-				if (!is_closed_path(neighbour))
-					neighbour->dst = neighbour->from->dst + 1;
-				enqueue(queue, (void *)neighbour);
-			}
-			++i;
-		}
+		apply_foreach_room_linked_to_ref(current, env, queue, &search_for_valid_neighbour);
 		ft_printf("} > ");
 	}
 }
 
-static void	reset_from_path(t_env *env, t_room *current)
+static void	reset_from_path(t_room *from, t_room *current, t_env *env)
 {
-	t_room	*from;
-
-	from = env->start;
+	if (!is_closed_path(current))
+		return ;
 	while (current != env->end)
 	{
 		current->from = from;
@@ -166,32 +157,25 @@ static void	reset_from_path(t_env *env, t_room *current)
 	}
 }
 
-static void	reset_from_paths(t_env *env)
+static void	initialize(t_env *env, t_queue *queue)
 {
-	int		i;
-
-	i = 0;
-	while (i < env->nb_room)
-	{
-		if (env->matrix[env->start->id][i] && (env->rooms_array[i]->flag & FL_CLOSE_PATH))
-			reset_from_path(env, env->rooms_array[i]);
-		++i;
-	}
-}
-
-static int	has_augmenting_path(t_env *env)
-{
-	t_queue	queue;
 	int		i;
 
 	i = 0;
 	while (i < env->nb_room)
 		env->rooms_array[i++]->visited = 0;
-	queue = (t_queue){0, 0};
-	enqueue(&queue, (void *)env->start);
+	*queue = (t_queue){0, 0};
+	enqueue(queue, (void *)env->start);
 	env->end->dst = INT_MAX;
 	env->start->visited = 1;
-	reset_from_paths(env);
+	apply_foreach_room_linked_to_ref(env->start, env, (void *)0, &reset_from_path);
+}
+
+static int	has_augmenting_path(t_env *env)
+{
+	t_queue	queue;
+
+	initialize(env, &queue);
 	bfs_max_flow(env, &queue);
 	if (env->end->dst == INT_MAX)
 		return (ERROR);
