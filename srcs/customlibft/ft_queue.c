9bdb5583476375520549eea6ae0bad64305db3df
Alexandre PION <apion@student.42.fr>
2019-05-28 20:54:11 +0200

    Improve ft_queue
    Create prequeue function to prepend an element to the queue

--- srcs/customlibft/ft_queue.c
+++ srcs/customlibft/ft_queue.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/30 21:36:32 by apion             #+#    #+#             */
-/*   Updated: 2019/05/01 08:40:49 by apion            ###   ########.fr       */
+/*   Updated: 2019/05/28 19:50:46 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,39 @@
 ** Not using ft_lstnew because I only need a queue of pointer, no need to memcpy
 */
 
-void	enqueue(t_queue *queue, void *content)
+static void	*create_node(void *content)
 {
 	t_list	*node;
 
 	node = (t_list *)malloc(sizeof(t_list));
 	if (!node)
-		return ;
+		return ((void *)0);
 	node->content = content;
 	node->content_size = 0;
 	node->next = 0;
+	return (node);
+}
+
+void	prequeue(t_queue *queue, void *content)
+{
+	t_list *node;
+
+	node = create_node(content);
+	if (!node)
+		return ;
+	node->next = queue->head;
+	queue->head = node;
+	if (!queue->tail)
+		queue->tail = node;
+}
+
+void	enqueue(t_queue *queue, void *content)
+{
+	t_list	*node;
+
+	node = create_node(content);
+	if (!node)
+		return ;
 	if (!queue->head)
 		queue->head = node;
 	if (queue->tail)
