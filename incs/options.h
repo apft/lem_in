70a9dda8f099843b18654e262f8cb1a4bbfd7283
Jalil KETTANI <jkettani@e2r2p3.42.fr>
2019-06-07 16:35:58 +0200

    Add -d option for debuging

--- incs/options.h
+++ incs/options.h
@@ -6,7 +6,7 @@
 /*   By: jkettani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 14:34:00 by jkettani          #+#    #+#             */
-/*   Updated: 2019/06/06 14:45:09 by jkettani         ###   ########.fr       */
+/*   Updated: 2019/06/07 16:04:03 by jkettani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "env.h"
 
 # define OP_ERR 0x1
+# define OP_DEBUG 0x2
 
 int		get_options(int ac, char **av, t_env *env);
 
--- srcs/options.c
+++ srcs/options.c
@@ -6,7 +6,7 @@
 /*   By: jkettani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 14:30:32 by jkettani          #+#    #+#             */
-/*   Updated: 2019/06/06 15:00:35 by jkettani         ###   ########.fr       */
+/*   Updated: 2019/06/07 16:19:00 by jkettani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 #include "env.h"
 #include "error.h"
 
-static int		update_flag(char c, t_uint *options)
+static int	update_flag(char c, t_uint *options)
 {
 	if (c == 'e')
 		*options |= OP_ERR;
+	else if (c == 'd')
+		*options |= OP_DEBUG;
 	else
 		return (ERR_INVALID_OPTION);
 	return (SUCCESS);
 }
 
-int		get_options(int ac, char **av, t_env *env)
+int			get_options(int ac, char **av, t_env *env)
 {
 	int			i;
 	int			j;
