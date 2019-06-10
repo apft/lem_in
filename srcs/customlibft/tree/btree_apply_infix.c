77cf09b37b9449b1fdb253c850b1a7d9535a1de7
Alexandre PION <apion@student.42.fr>
2019-06-10 21:14:16 +0200

    Improve rb_tree infix function

--- srcs/customlibft/tree/btree_apply_infix.c
+++ srcs/customlibft/tree/btree_apply_infix.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/02 13:12:51 by apion             #+#    #+#             */
-/*   Updated: 2019/06/10 20:58:58 by apion            ###   ########.fr       */
+/*   Updated: 2019/06/10 21:13:50 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ void	btree_apply_infix(t_rb_node *root, void *data, void (*fct)())
 		return ;
 	if (root->left)
 		btree_apply_infix(root->left, data, fct);
-	fct(root, data);
+	if (data)
+		fct(root, data);
+	else
+		fct(root);
 	if (root->right)
 		btree_apply_infix(root->right, data, fct);
 }
