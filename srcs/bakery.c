a1b7fbf4c51353d04858c83a039c3dcefaa72da0
Jalil KETTANI <jkettani@e2r2p3.42.fr>
2019-06-12 11:00:22 +0200

    Use generic merge sort to sort the array of rooms

--- srcs/bakery.c
+++ srcs/bakery.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 17:47:01 by apion             #+#    #+#             */
-/*   Updated: 2019/06/11 15:00:26 by apion            ###   ########.fr       */
+/*   Updated: 2019/06/12 10:59:38 by jkettani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 #include "cleaner.h"
 #include "error.h"
 #include "libft.h"
-#include <stdlib.h>
+#include "generic_merge_sort.h"
 
 static int	lst_to_array(t_env *env)
 {
 	t_list		*map;
 	int			i;
 
-	env->rooms_array = (t_room **)malloc(sizeof(t_room *) * env->nb_rooms);
+	env->rooms_array = (t_room **)ft_memalloc(sizeof(t_room *) * env->nb_rooms);
 	if (!env->rooms_array)
 		return (errno);
 	map = env->map;
@@ -40,7 +40,7 @@ static int	create_adjacency_matrix(t_env *env)
 {
 	int		i;
 
-	env->matrix = (int **)malloc(sizeof(int *) * env->nb_rooms);
+	env->matrix = (int **)ft_memalloc(sizeof(int *) * env->nb_rooms);
 	if (!env->matrix)
 		return (errno);
 	i = 0;
@@ -67,8 +67,35 @@ int			check_environment(t_env *env, int status)
 	return (SUCCESS);
 }
 
+int			cmp_rooms_name(t_room **room1, t_room **room2)
+{
+	return (ft_strcmp((*room1)->name, (*room2)->name));
+}
+
+int			create_ordered_rooms_array(t_env *env)
+{
+	t_array_args	args;
+	int				i;
+
+	if (lst_to_array(env) != SUCCESS)
+		return (ERR_ENV_LST_TO_ARRAY);
+	args = (t_array_args){env->rooms_array, sizeof(t_room *), env->nb_rooms,
+				&cmp_rooms_name};
+	if (array_merge_sort(&args) != SUCCESS)
+		return (ERR_ENV_ARRAY_SORT);
+	i = 0;
+	while (i < env->nb_rooms)
+	{
+		env->rooms_array[i]->id = i;
+		++i;
+	}
+	return (SUCCESS);
+}
+
 int			bake_environment(t_env *env, unsigned int *cmd_flag)
 {
+	int		status;
+
 	*cmd_flag ^= BLK_ROOM | BLK_TUBE;
 	if (!env->map)
 		return (ERR_ENV_EMPTY);
@@ -76,10 +103,9 @@ int			bake_environment(t_env *env, unsigned int *cmd_flag)
 		return (ERR_ENV_EMPTY_START);
 	if (!env->end)
 		return (ERR_ENV_EMPTY_END);
-	if (lst_to_array(env) != SUCCESS)
-		return (ERR_ENV_LST_TO_ARRAY);
-	if (array_room_merge_sort(env->rooms_array, env->nb_rooms) != SUCCESS)
-		return (ERR_ENV_ARRAY_SORT);
+	status = create_ordered_rooms_array(env);
+	if (status != SUCCESS)
+		return (status);
 	if (create_adjacency_matrix(env) != SUCCESS)
 		return (ERR_ENV_ADJACENCY_MATRIX);
 	return (SUCCESS);
